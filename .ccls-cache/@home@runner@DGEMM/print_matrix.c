bdc279e2d396b07052eec6515d0a2821bae604d0
Ari E. Kotler <jansherremway@gmail.com>
2024-10-29 20:32:52 +0000

    1.2

--- .ccls-cache/@home@runner@DGEMM/print_matrix.c
+++ .ccls-cache/@home@runner@DGEMM/print_matrix.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
 #include "matrix_operations.h"
+#include <stdio.h>
 
 void PrintMatrix(double *matrix, int size, int elements) {
-    for (int i = 0; i < elements; i++) {
-        printf("%f ", matrix[i]);
-    }
-    printf("\n");
+
+  for (int i = 0; i < elements; i++) {
+    printf("%f ", matrix[i]);
+  }
+  printf("\n");
 }
--- .ccls-cache/@home@runner@DGEMM/setup.c
+++ .ccls-cache/@home@runner@DGEMM/setup.c
@@ -1,16 +1,16 @@
+#include "matrix_operations.h"
 #include <stdlib.h>
 #include <time.h>
-#include "matrix_operations.h"
 
 void setup(double **A, double **B, double **C, int seed, int size) {
-    *A = (double *)malloc(size * size * sizeof(double));
-    *B = (double *)malloc(size * size * sizeof(double));
-    *C = (double *)malloc(size * size * sizeof(double));
+  *A = ((double *) (double *)malloc(size * size * sizeof(double)));
+  *B = ((double *) (double *)malloc(size * size * sizeof(double)));
+  *C = ((double *) (double *)malloc(size * size * sizeof(double)));
 
-    srand(seed);
-    for (int i = 0; i < size * size; i++) {
-        (*A)[i] = (double)rand() / RAND_MAX;
-        (*B)[i] = (double)rand() / RAND_MAX;
-        (*C)[i] = 0.0;
-    }
+  srand(seed);
+  for (int i = 0; i < size * size; i++) {
+    (*A)[i] = (double)rand() / RAND_MAX;
+    (*B)[i] = (double)rand() / RAND_MAX;
+    (*C)[i] = 0.0;
+  }
 }
